7970cdf9075347223ccd64dcb05c8c66f1d79e47
Satoshi <6world4trigger@gmail.com>
2024-09-02 02:57:51 +0800

    Update Tracker for the API protocol

--- src/live_tracker/client/trackers.py
+++ src/live_tracker/client/trackers.py
@@ -5,40 +5,65 @@ from collections import deque
 
 import requests
 
-from live_tracker.schema import Record, RecordValueT, APIRecord
+from live_tracker.schema import Record, RecordValueT, ClientRecord, DataSource, DataType
 
 
-DEFAULT_SERVER_URL = 'http://localhost:5000/record'
+DEFAULT_SERVER_URL = 'http://localhost:8000'
+DEFAULT_SEND_RECORD_ENDPOINT = DEFAULT_SERVER_URL + '/client/record'
+DEFAULT_ADD_DATA_SOURCE_ENDPOINT = DEFAULT_SERVER_URL + '/client/data_source'
 
 class Tracker(Generic[RecordValueT]):
     """
     Append added record to csv and post it to the server
     """
     # TODO: replace print with logging
-    def __init__(self, record_name: str, save_path: Path, server_url: str = DEFAULT_SERVER_URL, queue_maxsize: int = 300) -> None:
+    def __init__(self, record_name: str, save_path: Path, data_type: DataType, queue_maxsize: int = 300) -> None:
         self.save_path = save_path
-        self.server_url = server_url
         
         self.record_name = record_name
         self.record_index = 0
         
         self.record_queue: Deque[Record[RecordValueT]] = deque(maxlen=queue_maxsize)  # if queue is full, older records will be discarded
+        
+        self.data_source = DataSource(name=record_name, type=data_type, _data_path=save_path)
+        self.publish_data_source()
+    
+    def publish_data_source(self) -> None:
+        response = requests.post(DEFAULT_ADD_DATA_SOURCE_ENDPOINT, json=self.data_source.model_dump())
+        if response.status_code != 200:
+            raise ValueError('Failed to publish data source')
+        self.data_source.id = response.json()['data_source_id']
     
     def add(self, record_value: RecordValueT) -> None:
-        record = Record(index=self.record_index, name=self.record_name, value=record_value)
-        self._save_record(record)
-        self._post_pending_records()
-
-    def _post_pending_records(self) -> None:
-        body = [
-            APIRecord(record=record, datetime=datetime.now()).model_dump()
-            for record in self.record_queue
-        ]
-        response = requests.post(self.server_url, json=body)
+        if self.data_source.id is None:
+            raise ValueError('Data Record must be published before adding records')
+        record = Record(data_source_id=self.data_source.id, value=record_value)
+        # self._save_record(record)  # TODO: we don't save record unless we get an error response
+        # self._post_pending_records()
+        self._post_one_record(record)
+    
+    def _post_one_record(self, record: Record[RecordValueT]) -> None:
+        print(ClientRecord(record=record).model_dump_json())
+        response = requests.post(DEFAULT_SEND_RECORD_ENDPOINT, json=ClientRecord(record=record).model_dump())
         if response.status_code != 200:
-            print('Failed to post records in queue')
-        else:
-            self.record_queue.clear()
+            print(response)
+            print(response.text)
+            print('Failed to post record')
+
+    # TODO: this requires the server to accept multiple records in one request
+    # def _post_pending_records(self) -> None:
+        
+    #     body = [
+    #         ClientRecord(record=record, datetime=datetime.now()).model_dump_json()
+    #         for record in self.record_queue
+    #     ]
+    #     response = requests.post(DEFAULT_SEND_RECORD_ENDPOINT, json=body)
+    #     if response.status_code != 200:
+    #         print(response)
+    #         print(response.text)
+    #         print('Failed to post records in queue')
+    #     else:
+    #         self.record_queue.clear()
         
     def _save_record(self, record: Record[RecordValueT]) -> None:
         # TODO: add error handling (i think it's actually better to simply raise the error)
